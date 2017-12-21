f2773bd8ece0277703f772fb32a1261c0aa00688
tarakark <tarakaramadaiict@gmail.com>
2017-12-21 14:54:50 +0100

    added code for monolingual embeddings with softmax

--- code/monolingual_cv.py
+++ code/monolingual_cv.py
@@ -8,40 +8,41 @@ from keras.layers import Dense, Flatten, Dropout, Activation, Input, merge
 from keras.layers import Embedding, Convolution1D, MaxPooling1D
 from keras.layers import AveragePooling1D, LSTM, GRU
 from keras.utils import np_utils
-import sys, time, re
+import sys, time, re, glob
 from collections import defaultdict
 from gensim.utils import simple_preprocess
+from sklearn.model_selection import StratifiedKFold
+from sklearn.metrics import f1_score
 
 _white_spaces = re.compile(r"\s\s+")
-maxlen = 80
+
 maxchars = 200
-embedding_dims = 32
+embedding_dims = 100
 batch_size = 32
 nb_epoch = 10
 nb_filter = 128
 filter_length = 5
 pool_length = 32
 minfreq = 0
-data_path = "../data"
+data_path = sys.argv[1]
 minwordfreq = 15
-maxwordlen = 70
+maxwordlen = 400
+seed = 1234
 
-def read_data(data_file):
+def read_data():
     labels = []
     documents = []
-    with open(data_file, "r") as fp:
-        linenum = 0
-        for line in fp:
-            if len(line) < 3: # skip empty lines and ^Z at the end
-                continue
-            doc, label = line.strip().split("\t")
-            wrds = doc.split(" ")
-            if len(wrds) >70:
-                doc = " ".join(wrds[:70])
-            doc = _white_spaces.sub(" ", doc)
-            labels.append(label)
-            documents.append(doc)
-            linenum += 1
+    for data_file in glob.iglob(sys.argv[1]+"/*"):
+        doc = open(data_file, "r").read().strip()
+        wrds = doc.split(" ")
+        label = data_file.split("/")[-1].split(".txt")[0].split("_")[-1]
+        if label == "EMPTY": continue
+        if len(wrds) >= maxwordlen:
+            doc = " ".join(wrds[:maxwordlen])
+        doc = _white_spaces.sub(" ", doc)
+        labels.append(label)
+        documents.append(doc)
+        
     return (documents, labels)
 
 def char_tokenizer(s):
@@ -61,7 +62,7 @@ def getWords(D):
             max_features += 1
     return wordSet, max_features
 
-def getVocab(D):
+def getChars(D):
     charSet = defaultdict(int)
     max_features = 3
     for d in D:
@@ -107,13 +108,7 @@ def transform(D, vocab, minfreq, tokenizer="char"):
 print("Reading the training set... ", end="")
 sys.stdout.flush()
 pt = time.time()
-doc_train, y_train = read_data(data_path + "/train/task1-train.txt")
-print(time.time() - pt)
-
-print("Reading the test set... ", end="")
-sys.stdout.flush()
-pt = time.time()
-doc_test, y_test = read_data(data_path + "/gold/A.txt")
+doc_train, y_labels = read_data()
 print(time.time() - pt)
 
 print("Transforming the datasets... ", end="")
@@ -123,46 +118,58 @@ pt = time.time()
 word_vocab, max_word_features = getWords(doc_train)
 print("Number of features= ", max_word_features)
 x_word_train = transform(doc_train, word_vocab, minwordfreq, tokenizer="word")
-x_word_test = transform(doc_test, word_vocab, minwordfreq, tokenizer="word")
+
 print(len(x_word_train), 'train sequences')
-print(len(x_word_test), 'test sequences')
+
 print(time.time() - pt)
 
 print('Pad sequences (samples x time)')
 x_word_train = sequence.pad_sequences(x_word_train, maxlen=maxwordlen)
-x_word_test = sequence.pad_sequences(x_word_test, maxlen=maxwordlen)
 print('x_train shape:', x_word_train.shape)
-print('x_test shape:', x_word_test.shape)
+
 
 print("Transforming the labels... ", end="")
 sys.stdout.flush()
 pt = time.time()
-unique_labels = list(set(y_test))
+unique_labels = list(set(y_labels))
 print("Class labels = ",unique_labels)
 n_classes = len(unique_labels)
 indim = x_word_train.shape[1]
-y_train = [unique_labels.index(y) for y in y_train]
-y_test = [unique_labels.index(y) for y in y_test]
-y_train = np_utils.to_categorical(np.array(y_train), len(unique_labels))
-y_test = np_utils.to_categorical(np.array(y_test), len(unique_labels))
+y_labels = [unique_labels.index(y) for y in y_labels]
+
+y_train = np_utils.to_categorical(np.array(y_labels), len(unique_labels))
+
+print('y_train shape:', y_train.shape)
+
 print(time.time() - pt)
 
-print('Build model...')
+cv_accs, cv_f1 = [], []
+k_fold = StratifiedKFold(10)
 
-model = Sequential()
-model.add(Embedding(max_word_features, embedding_dims, input_length=maxwordlen, dropout=0.2))
-model.add(AveragePooling1D(pool_length=8))
-model.add(Flatten())
-#model.add(Dropout(0.5))
-#model.add(Dense(1024, activation='relu'))
-#model.add(Dropout(0.2))
-model.add(Dense(n_classes, activation='softmax'))
-model.summary()
-model.compile(loss='categorical_crossentropy',
-              optimizer='adadelta',
-              metrics=['accuracy'])
+for train, test in k_fold.split(x_word_train, y_labels):
+    #print("TRAIN:", train, "TEST:", test)
+    print('Build model...')
 
-model.fit(x_word_train, y_train,
-          batch_size=batch_size,
-          nb_epoch=nb_epoch,
-          validation_data=(x_word_test, y_test))
+    model = Sequential()
+    model.add(Embedding(max_word_features, embedding_dims, input_length=maxwordlen))
+    #model.add(GRU(50))
+    #model.add(AveragePooling1D(pool_length=8))
+    model.add(Flatten())
+    model.add(Dense(n_classes, activation='softmax'))
+    model.summary()
+    model.compile(loss='categorical_crossentropy',
+                optimizer='adadelta',
+                metrics=['accuracy'])
+
+
+    model.fit(x_word_train[train], y_train[train],
+              batch_size=batch_size,
+              nb_epoch=nb_epoch)
+
+    y_pred = model.predict_classes(x_word_train[test])
+    #print(y_pred, np.array(y_labels)[test], sep="\n")
+    cv_f1.append(f1_score(np.array(y_labels)[test], y_pred, average="weighted"))
+
+print("\nF1-scores", cv_f1,sep="\n")
+print("Average F1 scores", np.mean(cv_f1))
+
